66b14af813aaf5b8987bb1b9f4804b264f14fb41
ichlml <ichlamal8899@gmail.com>
2024-12-18 14:48:06 +0700

    first learn reactjs

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="flex justify-center bg-blue-300 min-h-screen items-center">
+        <button className="h-10 px-6 font-semibold rounded-md bg-black hover:bg-slate-100 hover:text-black text-white">
+          component button
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default App;
